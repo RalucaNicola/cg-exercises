7ed8c478dcf7dd3080e137d67225c0d5979ea135
Raluca Nicola <ralucagnicola@gmail.com>
2024-01-01 23:59:54 +0100

    Add a box to sceneview

--- src/script-arcgis-box.ts
+++ src/script-arcgis-box.ts
@@ -4,107 +4,13 @@ import SceneView from '@arcgis/core/views/SceneView';
 import Camera from '@arcgis/core/Camera';
 import Point from '@arcgis/core/geometry/Point';
 import * as externalRenderers from '@arcgis/core/views/3d/externalRenderers';
+import SpatialReference from '@arcgis/core/geometry/SpatialReference';
+import { mat4 } from 'gl-matrix';
 
 let view: SceneView;
 
-class ExternalRenderer {
 
 
-    // Shader attribute and uniform locations
-    programAttribVertexPosition: ArrayBuffer = null;
-    programAttribVertexColor: ArrayBuffer = null;
-    programUniformProjectionMatrix: ArrayBuffer = null;
-    programUniformModelViewMatrix: ArrayBuffer = null;
-    program: WebGLProgram = null;
-    view: SceneView = null;
-    localOrigin: Point = new Point({
-        x: -12978323.50406812,
-        y: 4017074.013392332,
-        z: 292.77160461955384
-    });
-
-    constructor(private view2: SceneView) {
-        this.view = view2;
-    }
-    setup(context: __esri.RenderContext) {
-        this.initShaders(context);
-    }
-    render(context: __esri.RenderContext) {
-        const gl = context.gl;
-
-        // Set some global WebGL state
-        // State will be reset between each render() call
-        gl.enable(gl.DEPTH_TEST);
-        gl.disable(gl.CULL_FACE);
-        gl.disable(gl.BLEND);
-        gl.useProgram(this.program);
-
-        gl.uniformMatrix4fv(
-            this.programUniformProjectionMatrix,
-            false,
-            context.camera.projectionMatrix
-        );
-    }
-
-    initShaders(context: __esri.RenderContext) {
-        const gl = context.gl;
-
-        // Initialize shaders
-        const vsSource = `
-    attribute vec4 a_Position;
-    attribute vec4 a_Color;
-    
-    uniform mat4 u_matrix;
-
-    varying vec4 v_Color;
-
-    void main() {
-        gl_Position = u_matrix * a_Position;
-        v_Color = a_Color;
-    }
-    `;
-
-        const fsSource = `
-    precision mediump float;
-    
-    varying vec4 v_Color;
-    
-    void main() {
-        gl_FragColor = v_Color;
-    }
-    `;
-
-        // Setup GLSL program
-        this.program = createProgram(gl, vsSource, fsSource);
-        if (!this.program) {
-            alert("Could not initialize shaders");
-        }
-        gl.useProgram(this.program);
-
-        // Look up vertex data locations
-        this.programAttribVertexPosition = gl.getAttribLocation(this.program, 'a_Position');
-        gl.enableVertexAttribArray(this.programAttribVertexPosition);
-        this.programAttribVertexColor = gl.getAttribLocation(this.program, 'a_Color');
-        gl.enableVertexAttribArray(this.programAttribVertexColor);
-        // Program uniforms
-        this.programUniformProjectionMatrix = gl.getUniformLocation(this.program, "uProjectionMatrix");
-        this.programUniformModelViewMatrix = gl.getUniformLocation(this.program, "uModelViewMatrix");
-
-    }
-
-    createVertexBuffer(gl: WebGL2RenderingContext, data: Array<number>) {
-        const buffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-
-        // We have filled vertex buffers in 64bit precision,
-        // convert to a format compatible with WebGL
-        const float32Data = new Float32Array(data);
-
-        gl.bufferData(gl.ARRAY_BUFFER, float32Data, gl.STATIC_DRAW);
-        return buffer;
-    }
-
-}
 
 function main() {
 
@@ -125,6 +31,7 @@ function main() {
             heading: 316,
             tilt: 85
         },
+        viewingMode: "local",
 
         qualityProfile: "high",
 
@@ -148,160 +55,193 @@ function main() {
 
 }
 
-main();
+class ExternalRenderer {
 
-function displayCube() {
 
-    const dim = 3;
-    // Get a WebGL context
-    const canvas = document.querySelector('canvas.webgl') as HTMLCanvasElement;
-
-    const gl = canvas.getContext('webgl') as WebGLRenderingContext;
-    if (!gl) {
-        console.error('WebGL not supported');
-    }
-
-    // Initialize shaders
-    const vsSource = `
-    attribute vec4 a_Position;
-    attribute vec4 a_Color;
-    
-    uniform mat4 u_matrix;
-
-    varying vec4 v_Color;
-
-    void main() {
-        gl_Position = u_matrix * a_Position;
-        v_Color = a_Color;
-    }
-    `;
-
-    const fsSource = `
-    precision mediump float;
-    
-    varying vec4 v_Color;
-    
-    void main() {
-        gl_FragColor = v_Color;
-    }
-    `;
-
-    // Setup GLSL program
-    const program = createProgram(gl, vsSource, fsSource);
-    if (!program) {
-        alert("Could not initialize shaders");
-    }
-    gl.useProgram(program);
-
-    // Look up vertex data locations
-    const positionLocation = gl.getAttribLocation(program, 'a_Position');
-    const colorLocation = gl.getAttribLocation(program, 'a_Color');
-
-    // Get uniform locations
-    const matrixLocation = gl.getUniformLocation(program, "u_matrix");
-    const timeLocation = gl.getUniformLocation(program, "u_time");
-
-    // Create a buffer to put positions in
-    const positionBuffer = gl.createBuffer();
-    // Bind it to array buffer
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-    // The vertices of a cube
-    const vertices = new Float32Array([
-        0.5, -0.5, 0.5, // 0
-        -0.5, -0.5, 0.5, // 1
-        -0.5, 0.5, 0.5, // 2
-        0.5, 0.5, 0.5, // 3
-        0.5, -0.5, -0.5, // 4
-        0.5, 0.5, -0.5, // 5
-        -0.5, 0.5, -0.5, // 6
-        -0.5, -0.5, -0.5 // 7
+    // Shader attribute and uniform locations
+    programAttribVertexPosition: ArrayBuffer = null;
+    programAttribVertexColor: ArrayBuffer = null;
+    vboPositions: WebGLBuffer = null;
+    vboColor: WebGLBuffer = null;
+    ibo: WebGLBuffer = null;
+    programUniformProjectionMatrix: ArrayBuffer = null;
+    programUniformModelViewMatrix: mat4 = null;
+    tempMatrix4: Float32Array = new Float32Array(16);
+    program: WebGLProgram = null;
+    view: SceneView = null;
+    // local origin x,y,z -> the cube will draw relative to this
+    localOrigin: Float32Array = new Float32Array([
+        - 12978278.649445374, 4017013.2729685362, 310.0
     ]);
-    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+    localOriginSR: __esri.SpatialReference = SpatialReference.WebMercator;
+    localOriginRender: any = null;
+    inputToRender: any = null;
 
-    gl.vertexAttribPointer(positionLocation, dim, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(positionLocation);
 
-    // Create a buffer to put colors in
-    const colorBuffer = gl.createBuffer();
-    // Bind it to array buffer
-    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-    //Put colors into buffer
-    const colors = new Uint8Array([
-        255, 0, 255,
-        0, 0, 255,
-        0, 255, 255,
-        255, 255, 255,
-        255, 0, 0,
-        255, 255, 0,
-        0, 255, 0,
-        0, 0, 0
-    ]);
-    gl.bufferData(gl.ARRAY_BUFFER, colors, gl.STATIC_DRAW);
+    constructor(private view2: SceneView) {
+        this.view = view2;
+    }
+    setup(context: __esri.RenderContext) {
+        this.initShaders(context);
+        this.initData(context);
+    }
+    render(context: __esri.RenderContext) {
+        console.log("render");
+        const { gl, camera } = context;
 
-    gl.vertexAttribPointer(colorLocation, dim, gl.UNSIGNED_BYTE, true, 0, 0);
-    gl.enableVertexAttribArray(colorLocation);
-
-    const indexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-
-    const indices = new Uint16Array([
-        0, 1, 2,
-        0, 2, 3,
-        0, 3, 5,
-        0, 5, 4,
-        0, 4, 1,
-        1, 4, 7,
-        1, 7, 6,
-        1, 6, 2,
-        2, 6, 3,
-        3, 6, 5,
-        4, 5, 6,
-        4, 6, 7
-    ]);
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-
-    let cameraAngle: number = degToRad(0);
-
-    function drawScene() {
-        const { width, height } = resizeCanvasToDisplaySize(canvas);
-
-        gl.viewport(0, 0, width, height);
-        gl.clearColor(0.0, 0.0, 0.0, 1.0); // Clear to black, fully opaque
-        gl.clearDepth(1.0); // Clear everything
-
-        // tell webgl to cull faces
-        // gl.enable(gl.CULL_FACE);
-
-        // turn on depth testing
+        // Set some global WebGL state
+        // State will be reset between each render() call
         gl.enable(gl.DEPTH_TEST);
+        gl.disable(gl.CULL_FACE);
+        gl.disable(gl.BLEND);
+        gl.useProgram(this.program);
 
-        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.vboPositions);
+        gl.vertexAttribPointer(this.programAttribVertexPosition, 3, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(this.programAttribVertexPosition);
 
-        // Compute projection matrix
-        const aspect = width / height;
-        const zNear = 1;
-        const zFar = -1;
-        const projectionMatrix = twgl.m4.perspective(degToRad(60), aspect, zNear, zFar);
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.vboColor);
+        gl.vertexAttribPointer(this.programAttribVertexColor, 3, gl.UNSIGNED_BYTE, true, 0, 0);
+        gl.enableVertexAttribArray(this.programAttribVertexColor);
 
-        // Compute a matrix for the camera
-        let cameraMatrix = twgl.m4.rotationY(cameraAngle);
-        cameraMatrix = twgl.m4.translate(cameraMatrix, [0, 0, 2]);
-        const viewMatrix = twgl.m4.inverse(cameraMatrix);
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.ibo);
 
-        // Compute a view projection matrix
-        const viewProjectionMatrix = twgl.m4.multiply(projectionMatrix, viewMatrix);
+        gl.uniformMatrix4fv(
+            this.programUniformProjectionMatrix,
+            false,
+            context.camera.projectionMatrix
+        );
 
-        gl.uniformMatrix4fv(matrixLocation, false, viewProjectionMatrix);
+        mat4.identity(this.tempMatrix4);
+        mat4.translate(this.tempMatrix4, this.tempMatrix4, this.localOriginRender);
+        mat4.multiply(this.tempMatrix4, camera.viewMatrix, this.tempMatrix4);
+        gl.uniformMatrix4fv(
+            this.programUniformModelViewMatrix,
+            false,
+            this.tempMatrix4
+        );
 
         gl.drawElements(gl.TRIANGLES, 36, gl.UNSIGNED_SHORT, 0);
     }
-    drawScene();
-    document.getElementById('angleSlider').addEventListener('input', function (this: HTMLInputElement) {
-        cameraAngle = degToRad(parseInt(this.value));
-        drawScene();
-    });
+
+    initShaders(context: __esri.RenderContext) {
+        const gl = context.gl;
+
+        // Initialize shaders
+        const vsSource = `
+            attribute vec4 a_position;
+            attribute vec4 a_color;
+            uniform mat4 u_projectionMatrix;
+            uniform mat4 u_modelViewMatrix;
+
+            varying vec4 v_color;
+
+            void main() {
+                gl_Position = u_projectionMatrix * u_modelViewMatrix * a_position;
+                v_color = a_color;
+            }
+        `;
+
+        const fsSource = `
+            precision mediump float;
+            varying vec4 v_color;    
+            void main() {
+                gl_FragColor = v_color;
+            }
+        `;
+
+        // Setup GLSL program
+        this.program = createProgram(gl, vsSource, fsSource);
+        if (!this.program) {
+            alert("Could not initialize shaders");
+        }
+
+        // Program attributes
+        this.programAttribVertexPosition = gl.getAttribLocation(this.program, 'a_position');
+        gl.enableVertexAttribArray(this.programAttribVertexPosition);
+        this.programAttribVertexColor = gl.getAttribLocation(this.program, 'a_color');
+        gl.enableVertexAttribArray(this.programAttribVertexColor);
+        // Program uniforms
+        this.programUniformProjectionMatrix = gl.getUniformLocation(this.program, "u_projectionMatrix");
+        this.programUniformModelViewMatrix = gl.getUniformLocation(this.program, "u_modelViewMatrix");
+
+    }
+
+    initData(context: __esri.RenderContext) {
+        const gl = context.gl;
+
+        const { positions, colors, indices } = createCubeVertices(10);
+
+        this.programAttribVertexPosition = gl.getAttribLocation(this.program, "a_position");
+        this.vboPositions = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.vboPositions);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+        this.programAttribVertexColor = gl.getAttribLocation(this.program, "a_color");
+        this.vboColor = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.vboColor);
+        gl.bufferData(gl.ARRAY_BUFFER, new Uint8Array(colors), gl.STATIC_DRAW);
+
+
+        this.ibo = gl.createBuffer();
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.ibo);
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
+
+        this.localOriginRender = externalRenderers.toRenderCoordinates(
+            view,
+            this.localOrigin,
+            0,
+            this.localOriginSR,
+            new Float32Array(3),
+            0,
+            1
+        );
+
+    }
+
 }
+
+function createCubeVertices(size: number) {
+    const k = size / 2;
+    const positions = new Float32Array([
+        k, -k, k, // 0
+        k, k, k, // 1
+        -k, k, k,   // 2
+        -k, -k, k, // 3
+        k, -k, -k, // 4
+        k, k, -k,   // 5
+        -k, k, -k,  // 6
+        -k, -k, -k  // 7
+    ]);
+    const colors = new Uint8Array([
+        230, 188, 223, // 0
+        76, 142, 232, // 1
+        76, 142, 232, // 2
+        230, 188, 223, // 3
+        230, 188, 223, // 4
+        76, 142, 232, // 5
+        76, 142, 232, // 6
+        230, 188, 223 // 7
+    ]);
+    const indices = new Uint16Array([
+        0, 1, 2,
+        0, 2, 3,
+        0, 5, 1,
+        0, 4, 5,
+        0, 7, 4,
+        0, 3, 7,
+        4, 6, 5,
+        4, 7, 6,
+        1, 5, 6,
+        1, 6, 2,
+        3, 6, 7,
+        3, 2, 6
+    ]);
+    return { positions, colors, indices };
+
+}
+
+main();
 
 
 /**
@@ -321,7 +261,7 @@ function createShader(gl: WebGLRenderingContext, src: string, type: number): Web
     gl.shaderSource(shader, src);
     gl.compileShader(shader);
     if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-        alert("Error compiling shader: " + gl.getShaderInfoLog(shader));
+        console.error("Error compiling shader: " + gl.getShaderInfoLog(shader));
         return;
     }
     return shader;
